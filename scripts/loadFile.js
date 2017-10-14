549c95df2c98a6d2b84ddf55842f554f883cc3ad
gAldeia <guilherme.aldeia@aluno.ufabc.edu.br>
2017-10-14 22:11:05 +0000

    entrada pode ser separada por tabulação também

--- scripts/loadFile.js
+++ scripts/loadFile.js
@@ -89,7 +89,7 @@ function loadHandler(inputData){
 	
 	for (var i=0; i<allTextLines.length; i++){
 	
-		var data = allTextLines[i].split(/\ |,/);
+		var data = allTextLines[i].split(/\ |,|\t/);
 		var tarr = [];
 		
 		for (var j=0; j<data.length; j++){
