429bdbd661cffb716c9c20eaf0bea00569ce66a8
gAldeia <guilherme.aldeia@aluno.ufabc.edu.br>
2017-11-25 13:14:19 +0000

    melhoria no código da página de exemplos

--- scripts/Generator.js
+++ scripts/Generator.js
@@ -2,45 +2,68 @@
 
 
 function noise(){
-    if (document.getElementById("noise").checked){
-        if (Math.random()>0.5)
-            return Math.random()*0.3;
-        else
-            return -Math.random()*0.3;
-    }
+    if (document.getElementById("noise").checked)
+        return (Math.random()*0.6) -0.3;
     return 0;
 }
 
-function Eval(range, nOfVars){
-    let display = "<table style='width: 300px'><tr><th>f(x)</th>";
+function generateLines(){
+    let mat = createMatrix(document.getElementById("expressionInput").value, document.getElementById("range").value, true);
 
-    for(let i=1; i<=nOfVars; i++){
-        display += "<th>x"+i+"</th>";
-    }
-    display+="</tr>";
-
-    for(let i=1; i<range; i++){
-        let exp = document.getElementById("expressionInput").value;
-
-        let lines = "";
-        for(let j=1; j<=nOfVars; j++){
-            exp = exp.split("x"+j).join(i);
-            lines+="<td>"+ (i) +"</td>";
-        }
-        let result = eval(exp)+noise();
-        display+="<tr><td>"+result+"</td>"+lines+"</tr>";
-    }
-    document.getElementById("expressionOutput").innerHTML=display+"</table>";
+    document.getElementById("expressionOutput").innerHTML = createTable(mat, document.getElementById("expressionInput").value);
 }
 
-function generateLines(){
-    let range = document.getElementById("range").value;
+function createMatrix(expression, range, useNoise){
     let nOfVars = 0;
 
     for(let i=0; i<10; i++){
-        let expressionToCount = document.getElementById("expressionInput").value;
-        if (expressionToCount.split("x"+i).length - 1)
+        if (expression.split("x"+i).length - 1)
             nOfVars++;
     }
-    Eval(range, nOfVars);
+    let matrix = [ ];
+    for(let i=0; i<range; i++){
+        let expressionAux = expression.slice(0);
+        let line = [ ];
+        for(let j=0; j<nOfVars; j++){
+            line.push(Math.ceil(Math.random()*i)+ i);
+            expressionAux = expressionAux.split("x"+j).join(line[j]);
+        }
+        let result = eval(expressionAux);
+        if (useNoise)
+            result += noise();
+
+        line.push(result.toFixed(2));
+        matrix.push(line);
+    }
+    return matrix;
 }
+
+function createTable(matrix, tableName){
+
+    let header = '<tr>';
+    for(let i=0; i<matrix[0].length-1; i++)
+        header+= '<th>' + ('x' + i) + '</th>';
+    header += '<th>f(x)</th><tr>';
+
+    let lines = '';
+    for(let i=0; i<matrix.length; i++){
+        lines += '<tr>';
+        for(let j=0; j<matrix[i].length; j++)
+            lines+='<td>' + matrix[i][j] + '</td>';
+        lines +='</tr>';
+    }
+
+    return '<table style="width:' + matrix[0].length*75 + 'px">'+header+lines+'<caption class="text-center">'+tableName+'</caption></table>';
+}
+
+(function(){
+    tableHolder = document.getElementById("tableHolder");
+    
+    let expressions = ["x0*x0", "Math.sqrt(x0)", "Math.sin(x0)", "x0 + x1*x1", "x0*x1", "Math.sin(x0) + x1*x1"];
+    let expressionsNames = ["f(x) = x^2", "f(x) = x^(1/2)", "f(x) = sin(x)", "f(x0, x1) = x0 + x1^2", "f(x0, x1) = x0*x1", "f(x0, x1) = sin(x0) + x1^2"];
+
+    for(let i=0; i<expressions.length; i++){
+        let mat = createMatrix(expressions[i], 10, false);
+        tableHolder.innerHTML += '<div class="col-lg-3"><p>'+createTable(mat, expressionsNames[i])+'</p></div>';
+    }
+})();
