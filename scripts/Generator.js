989fe99a4352d318b6c78d789b27e89981a45dab
gAldeia <guilherme.aldeia@aluno.ufabc.edu.br>
2017-11-26 20:34:59 +0000

    fix

--- scripts/Generator.js
+++ scripts/Generator.js
@@ -52,7 +52,7 @@ function createMatrix(expression, range, useNoise, strictlyPositive){
         }
         catch(err){
             document.getElementById("expressionOutput").innerHTML="<div class='alert alert-danger'><p class='text-justify'><strong>Atenção!</strong> Alguma coisa está errada. Verifique se não esqueceu algum operador entre dois números e se funções matemáticas (seno, cosseno, raiz, etc) estão de acordo com a sintaxe da biblioteca Math do javascript (Math.sin(), Math.cos(), Math.sqrt()).</p></div>";
-            return;
+            return [];
         }
         if (useNoise) result += noise();
 
