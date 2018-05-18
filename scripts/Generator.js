677f10a6a3b48b6291be09009c992746166c0e66
gAldeia <guilherme.aldeia@aluno.ufabc.edu.br>
2018-05-18 18:49:28 +0000

    local storage input examples

--- scripts/Generator.js
+++ scripts/Generator.js
@@ -89,7 +89,7 @@ function createTable(matrix, tableName){
     let header = '<tbody><tr>';
     for(let i=0; i<matrix[0].length-1; i++)
         header+= '<th>' + ('x' + i) + '<\/th>';
-    header += '<th>f(X)<\/th><tr>';
+    header += '<th>f(X)<\/th><\/tr>\n';
 
     let lines = '';
     for(let i=0; i<matrix.length; i++){
